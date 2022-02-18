27dca389e2c6c34b0c7332db9587cdc75e8c1d88
Michael Moser <moser.michael@gmail.com>
2022-02-18 15:13:16 +0200

    try to fix build

--- build/build-lang-in-docker.sh
+++ build/build-lang-in-docker.sh
@@ -25,22 +25,16 @@ DID_CHANGE=$(git diff description_cache.json)
 git config --global user.email "a@gmail.com"
 git config --global user.name "MoserMichael"
 
-if [[ "$DID_CHANGE" != "" ]]; then
-
-    echo "*** pushing changed file ***"
- 
-    git add description_cache.json
-    git commit -m "automatic build - identify language of description with fasttext $(date)"
-    expect -f /ex
-else
+if [[ "$DID_CHANGE" == "" ]]; then
     echo "*** the file didn't change ***"
+    exit 1
 fi
 
-# generate some action in the main repository, so that the CI job will not get disabled.
-git checkout master
-date >> ci-runs.txt
-git add ci-runs.txt
-git commit -m "automatic build - add language tag $(date)"
+
+echo "*** pushing changed file ***"
+
+git add description_cache.json
+git commit -m "automatic build - identify language of description with fasttext $(date)"
 expect -f /ex
 
 popd
